8dbd30cf9c2bc074a8be0d9bf1d58dffc2c69eba
umayer10-AI <mdumayer10@gmail.com>
2026-06-26 22:46:49 +0600

    add

--- User.js
+++ User.js
@@ -5,45 +5,46 @@ const addressSchema = new mongoose.Schema({
         city: String
     })
 
-const userSchema = new mongoose.Schema({
-    name: String,
-    age: {
-        type: Number,
-        min: 1,
-        max: 100,
-        validate: {
-            validator: v => v % 2 === 0,
-            message: props => `${props.value} is not an even number`
+const userSchema = new mongoose.Schema(
+    {
+        name: String,
+        age: {
+            type: Number,
+            min: 1,
+            max: 100,
+            validate: {
+                validator: v => v % 2 === 0,
+                message: props => `${props.value} is not an even number`
+            },
         },
-    },
-    email: {
-        type: String,
-        required: true,
-        minLength: 10,
-        lowercase: true,
-        // uppercase: true,
-    },
+        email: {
+            type: String,
+            required: true,
+            minLength: 10,
+            lowercase: true,
+            // uppercase: true,
+        },
 
-    // createdAt: {
-    //     type: Date,
-    //     immutable: true,
-    //     // default: new Date(),
-    //     default: () => Date.now(),
-    // },
-    // updatedAt: {
-    //     type: Date,
-    //     // default: new Date(),
-    //     default: () => Date.now(),
-    // },
-    bestFriend: mongoose.SchemaTypes.ObjectId,
-    hobbies: [String],
-    address: addressSchema,
-},
-{
-    timestamps: true,
-    // versionKey: false,
-    strict: true,
-},
+        // createdAt: {
+        //     type: Date,
+        //     immutable: true,
+        //     // default: new Date(),
+        //     default: () => Date.now(),
+        // },
+        // updatedAt: {
+        //     type: Date,
+        //     // default: new Date(),
+        //     default: () => Date.now(),
+        // },
+        bestFriend: mongoose.SchemaTypes.ObjectId,
+        hobbies: [String],
+        address: addressSchema,
+    },
+    {
+        timestamps: true,
+        // versionKey: false,
+        strict: true,
+    },
 )
 
 module.exports = mongoose.model("User", userSchema)
